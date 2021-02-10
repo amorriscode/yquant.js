779c750b37aaee3b70e5aaeee652e86fb9a573ff
Anthony Morris <amorriscode@gmail.com>
2021-02-09 21:32:25 -0800

    Add the ability to get S&P 500 list

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
-export const BASE_URL = 'https://query1.finance.yahoo.com/v8/finance/chart/'
+export const YAHOO_FINANCE_CHART_URL =
+  'https://query1.finance.yahoo.com/v8/finance/chart/'
 
 export const NASDAQ_URL = 'ftp.nasdaqtrader.com'
+
+export const SP500_URL =
+  'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import ticker from './ticker'
+import market, { sp500 } from './market'
+
+sp500()
 
 export default {
-  ticker,
+  ...ticker,
+  ...market,
 }
