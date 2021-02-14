b93692b336110ce59d197f29a5bd39b17153a707
Anthony Morris <anthony@amorrissound.com>
2021-02-14 10:42:13 -0800

    Add ability to get stats (#5)
    * Add ability to get stats
    
    * Remove non-existent export

--- src/ticker.js
+++ src/ticker.js
@@ -1,10 +1,37 @@
-import { getChartData } from './yfinance'
+import got from 'got'
 
-export async function getTicker(ticker) {
-  const tickerData = await getChartData(ticker)
-  return tickerData
+import { YAHOO_FINANCE_CHART_URL, YAHOO_FINANCE_QUOTE_URL } from './constants'
+import { parseYahooFinanceSummary, parseYahooFinanceStats } from './utils/data'
+
+export async function getChartData(
+  ticker,
+  params = { startDate: 7223400, interval: '1d' }
+) {
+  return await got(`${YAHOO_FINANCE_CHART_URL}/${ticker}`, {
+    searchParams: {
+      interval: params.interval,
+      period1: new Date(params.startDate).getTime(),
+      period2: params.endDate
+        ? new Date(params.endDate).getTime()
+        : new Date().getTime(),
+    },
+  }).json()
+}
+
+export async function getSummary(ticker) {
+  const response = await got(`${YAHOO_FINANCE_QUOTE_URL}/${ticker}?p=${ticker}`)
+  return parseYahooFinanceSummary(response.body)
+}
+
+export async function getStats(ticker) {
+  const response = await got(
+    `${YAHOO_FINANCE_QUOTE_URL}/${ticker}/key-statistics?p=${ticker}`
+  )
+  return parseYahooFinanceStats(response.body)
 }
 
 export default {
-  getTicker,
+  getChartData,
+  getSummary,
+  getStats,
 }
--- src/utils/data.js
+++ src/utils/data.js
@@ -46,42 +46,264 @@ export function parseNasdaqData(data) {
 }
 
 export function parseYahooFinanceSummary(html) {
-  const htmlTree = parse(html)
-  const quoteSummary = htmlTree.querySelector('#quote-summary')
-
   const summary = {}
 
-  quoteSummary.querySelectorAll('table').forEach((table) => {
-    table
-      .querySelectorAll('tr')
-      .map((tr) => tr.childNodes.map(getTableText).filter(Boolean))
-      .forEach(([header, value]) => {
+  parse(html)
+    .querySelector('#quote-summary')
+    .querySelectorAll('table')
+    .forEach((table) => {
+      table
+        .querySelectorAll('tr')
+        .map((tr) => tr.childNodes.map(getTableText).filter(Boolean))
+        .forEach(([header, value]) => {
+          let key
+
+          header = header.replace(/ \([^)]*\)/g, '')
+
+          // Handle some special key cases
+          switch (header) {
+            case 'Day&#x27;s Range':
+              key = 'daysRange'
+              break
+            case '52 Week Range':
+              key = 'fiftyTwoWeekRange'
+              break
+            case 'Beta':
+              key = 'betaFiveYearMonthly'
+              break
+            case '1y Target Est':
+              key = 'oneYearTarget'
+              break
+            default:
+              key = camelize(header)
+          }
+
+          summary[key] = value
+        })
+    })
+
+  return summary
+}
+
+export function parseYahooFinanceStats(html) {
+  const htmlTree = parse(html)
+
+  const stats = {
+    valuationMeasures: {},
+    financialHighlights: {
+      fiscalYear: {},
+      profitability: {},
+      managementEffectiveness: {},
+      incomeStatement: {},
+      balanceSheet: {},
+      cashFlowStatement: {},
+    },
+    tradingInformation: {
+      stockPriceHistory: {},
+      shareStatistics: {},
+      dividensAndSplits: {},
+    },
+  }
+
+  const transformers = [
+    {
+      headers: [
+        'Market Cap',
+        'Enterprise Value',
+        'Trailing P/E',
+        'Forward P/E',
+        'PEG Ratio',
+        'Price/Sales',
+        'Price/Book',
+        'Enterprise Value/Revenue',
+        'Enterprise Value/EBITDA',
+      ],
+      transform: (header, value) => {
+        header = header.replace(/ \([^)]*\)/g, '')
+        stats.valuationMeasures[camelize(header)] = value
+      },
+    },
+    {
+      headers: ['Fiscal Year Ends', 'Most Recent Quarter'],
+      transform: (header, value) => {
+        header = header.replace(/ \([^)]*\)/g, '')
+        stats.financialHighlights.fiscalYear[camelize(header)] = value
+      },
+    },
+    {
+      headers: ['Profit Margin', 'Operating Margin'],
+      transform: (header, value) => {
+        header = header.replace(/ \([^)]*\)/g, '')
+        stats.financialHighlights.profitability[camelize(header)] = value
+      },
+    },
+    {
+      headers: ['Return on Assets', 'Return on Equity'],
+      transform: (header, value) => {
+        header = header.replace(/ \([^)]*\)/g, '')
+        stats.financialHighlights.managementEffectiveness[
+          camelize(header)
+        ] = value
+      },
+    },
+    {
+      headers: [
+        'Revenue',
+        'Revenue Per Share',
+        'Quarterly Revenue Growth',
+        'Gross Profit',
+        'EBITDA',
+        'Net Income Avi to Common',
+        'Diluted EPS',
+        'Quarterly Earnings Growth',
+      ],
+      transform: (header, value) => {
+        header = header.replace(/ \([^)]*\)/g, '')
+        stats.financialHighlights.incomeStatement[camelize(header)] = value
+      },
+    },
+    {
+      headers: [
+        'Total Cash',
+        'Total Cash Per Share',
+        'Total Debt',
+        'Total Debt/Equity',
+        'Current Ratio',
+        'Book Value Per Share',
+      ],
+      transform: (header, value) => {
+        header = header.replace(/ \([^)]*\)/g, '')
+        stats.financialHighlights.balanceSheet[camelize(header)] = value
+      },
+    },
+    {
+      headers: ['Operating Cash Flow', 'Levered Free Cash Flow'],
+      transform: (header, value) => {
+        header = header.replace(/ \([^)]*\)/g, '')
+        stats.financialHighlights.cashFlowStatement[camelize(header)] = value
+      },
+    },
+    {
+      headers: [
+        'Beta (5Y Monthly)',
+        '52-Week Change',
+        'S&P500 52-Week Change',
+        '52 Week High',
+        '52 Week Low',
+        '50-Day Moving Average',
+        '200-Day Moving Average',
+      ],
+      transform: (header, value) => {
         let key
 
-        // Handle some special key cases
         switch (header) {
-          case 'Day&#x27;s Range':
-            key = 'daysRange'
-            break
-          case '52 Week Range':
-            key = 'fiftyTwoWeekRange'
-            break
           case 'Beta (5Y Monthly)':
             key = 'betaFiveYearMonthly'
             break
-          case '1y Target Est':
-            key = 'oneYearTarget'
+          case '52-Week Change':
+            key = 'fiftyTwoWeekChange'
+            break
+          case 'S&P500 52-Week Change':
+            key = 'spFiftyTwoWeekChange'
+            break
+          case '52 Week High':
+            key = 'fiftyTwoWeekHigh'
+            break
+          case '52 Week Low':
+            key = 'fiftyTwoWeekLow'
+            break
+          case '50-Day Moving Average':
+            key = 'fiftyDayMovingAverage'
+            break
+          case '200-Day Moving Average':
+            key = 'twoHundredDayMovingAverage'
             break
           default:
             key = camelize(header)
         }
 
-        // Replace janky TTM from the few keys that have it
-        key = key.replace('Ttm)', '')
+        stats.tradingInformation.stockPriceHistory[key] = value
+      },
+    },
+    {
+      headers: [
+        'Avg Vol (3 month)',
+        'Avg Vol (10 day)',
+        'Shares Outstanding',
+        'Float',
+        '% Held by Insiders',
+        '% Held by Institutions',
+        'Shares Short',
+        'Short Ratio',
+        'Short % of Float',
+        'Short % of Shares Outstanding',
+        'Shares Short',
+      ],
+      transform: (header, value) => {
+        let key
+        header = header.replace('%', 'Percent')
 
-        summary[key] = value
-      })
-  })
+        switch (header) {
+          case 'Avg Vol (3 month)':
+            key = 'avgVolThreeMonth'
+            break
+          case 'Avg Vol (10 day)':
+            key = 'avgVolTenDay'
+            break
+          default:
+            header = header.replace(/ \([^)]*\)/g, '')
+            key = camelize(header)
+        }
 
-  return summary
+        stats.tradingInformation.shareStatistics[key] = value
+      },
+    },
+    {
+      headers: [
+        'Forward Annual Dividend Rate',
+        'Forward Annual Dividend Yield',
+        'Trailing Annual Dividend Rate',
+        'Trailing Annual Dividend Yield',
+        '5 Year Average Dividend Yield',
+        'Payout Ratio',
+        'Dividend Date',
+        'Ex-Dividend Date',
+        'Last Split Factor',
+        'Last Split Date',
+      ],
+      transform: (header, value) => {
+        const key =
+          header === '5 Year Average Dividend Yield'
+            ? 'fiveYearAverageDividendYield'
+            : camelize(header)
+
+        stats.tradingInformation.dividensAndSplits[key] = value
+      },
+    },
+  ]
+
+  const rows = htmlTree
+    .querySelectorAll('table')
+    .flatMap((table) =>
+      table
+        .querySelectorAll('tr')
+        .map((tr) => tr.childNodes.map(getTableText).filter(Boolean))
+    )
+
+  for (const [header, value] of rows) {
+    const headerExists = (h) => header.includes(h)
+
+    // Remove footnotes
+    let transformedHeader = header.replace(/ \d+$/, '')
+
+    // Transform headers
+    for (const { headers, transform } of transformers) {
+      if (headers.some(headerExists)) {
+        transform(transformedHeader, value)
+        continue
+      }
+    }
+  }
+
+  return stats
 }
