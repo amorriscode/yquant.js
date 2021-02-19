5f6c92c8e193be2d0a6bcd506b8d1130af5235ec
Anthony Morris <amorriscode@gmail.com>
2021-02-18 21:32:13 -0800

    Remove helper function

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import ticker from './ticker'
 import market from './market'
 
-async function ok() {
-  console.log(await ticker.cashFlow('aapl'))
-}
-
-ok()
-
 export default {
   ...ticker,
   ...market,
